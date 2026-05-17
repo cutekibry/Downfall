3044a277496eca824eaf285e12bea0bcad9f76a1
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-17 21:20:09 +0800

    feat: add new tip for Awakened

--- AwakenedCode/CustomEnums/AwakenedTip.cs
+++ AwakenedCode/CustomEnums/AwakenedTip.cs
@@ -5,6 +5,7 @@ namespace Awakened.AwakenedCode.CustomEnums;
 
 public static class AwakenedTip
 {
+    [CustomEnum] public static StaticHoverTip Awaken;
     [CustomEnum] public static StaticHoverTip Conjure;
     [CustomEnum] public static StaticHoverTip Chant;
     [CustomEnum] public static StaticHoverTip Drained;
