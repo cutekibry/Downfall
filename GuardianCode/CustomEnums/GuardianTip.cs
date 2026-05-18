c8bb3aa685a99b3efb68281f62cc345694a03946
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-18 19:05:04 +0800

    feat(GuardianTip): add new hover tips

--- GuardianCode/CustomEnums/GuardianTip.cs
+++ GuardianCode/CustomEnums/GuardianTip.cs
@@ -6,8 +6,12 @@ namespace Guardian.GuardianCode.CustomEnums;
 public static class GuardianTip
 {
     [CustomEnum] public static StaticHoverTip Accelerate;
-    [CustomEnum] public static StaticHoverTip Stasis;
+    [CustomEnum] public static StaticHoverTip Aggravate;
     [CustomEnum] public static StaticHoverTip Brace;
-    [CustomEnum] public static StaticHoverTip Tick;
+    [CustomEnum] public static StaticHoverTip DefensiveMode;
+    [CustomEnum] public static StaticHoverTip Package;
     [CustomEnum] public static StaticHoverTip Polish;
+    [CustomEnum] public static StaticHoverTip Socket;
+    [CustomEnum] public static StaticHoverTip Stasis;
+    [CustomEnum] public static StaticHoverTip Tick;
 }
