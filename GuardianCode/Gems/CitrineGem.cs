663db3b3a08325266befcf87497b7db69b94647c
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-19 00:58:37 +0800

    fix(CitrineGem): use Energy instead of Gem
    Gem is deprecated as we can't use `{Gem:energyIcons()}` in localization.

--- GuardianCode/Gems/CitrineGem.cs
+++ GuardianCode/Gems/CitrineGem.cs
@@ -1,8 +1,6 @@
 using Godot;
 using Guardian.GuardianCode.Core;
-using Guardian.GuardianCode.DynamicVars;
 using Guardian.GuardianCode.Events;
-using Guardian.GuardianCode.Extensions;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
@@ -20,7 +18,7 @@ public class CitrineGem : GemModel
 
     protected override async Task OnPlay(PlayerChoiceContext ctx, CardPlay? cardPlay)
     {
-        var effect = GuardianHook.ModifyGemEffect(CombatState, this, DynamicVars.Gem().BaseValue, Card);
+        var effect = GuardianHook.ModifyGemEffect(CombatState, this, DynamicVars.Energy.BaseValue, Card);
         await PlayerCmd.GainEnergy(effect, Player);
     }
 }
