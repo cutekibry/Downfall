d57a67de8a346dd365d93a88ea04d0486c644f00
lamali <lamali292@gmail.com>
2026-05-30 20:47:50 +0200

    Implement Recollect card functionality: add block and Slurp mechanics, integrate card play effects

--- SlimeBossCode/Cards/Uncommon/Recollect.cs
+++ SlimeBossCode/Cards/Uncommon/Recollect.cs
@@ -2,6 +2,7 @@ using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using SlimeBoss.SlimeBossCode.Core;
+using SlimeBoss.SlimeBossCode.Extensions;
 
 namespace SlimeBoss.SlimeBossCode.Cards.Uncommon;
 
@@ -10,10 +11,13 @@ public class Recollect : SlimeBossCardModel
 {
     public Recollect() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
     {
+        WithBlock(8, 3);
+        this.WithSlurp(1);
     }
-
-    // TODO: Implement
+    
     protected override async Task OnPlay(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
+        await CommonActions.CardBlock(this, cardPlay);
+        await SlimeBossCmd.Slurp(this);
     }
 }
