4bea506bd9fb5fed165ed92d6b3b222163eb9331
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-06-13 19:21:43 +0800

    fix(Planning): should shuffle when no cards

--- GuardianCode/Cards/Common/Planning.cs
+++ GuardianCode/Cards/Common/Planning.cs
@@ -2,6 +2,7 @@ using BaseLib.Utils;
 using Guardian.GuardianCode.Core;
 using Guardian.GuardianCode.CustomEnums;
 using Guardian.GuardianCode.Interfaces;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 
@@ -21,9 +22,12 @@ public class Planning : GuardianCardModel, IGemSocketCard
 
     protected override async Task OnPlay(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
-        var cards = Owner.GetDraw().Take(DynamicVars.Cards.IntValue).ToList();
-        foreach (var card in cards)
+        for (int i = 0; i < DynamicVars.Cards.IntValue; i++)
         {
+            if (!Owner.GetDraw().Any())
+                await CardPileCmd.Shuffle(ctx, Owner);
+            var card = Owner.GetDraw().ToList().FirstOrDefault();
+            if (card == null) return;
             await GuardianCmd.PutIntoStasis(card, ctx, this);
         }
     }
