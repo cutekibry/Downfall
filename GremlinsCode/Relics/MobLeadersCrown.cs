b5bfe3a2bf69835f1d344b8e27e2cbf76c28d6d7
ShenanigansBunul <sergiiusalceanu@gmail.com>
2026-06-17 18:19:29 +0300

    add turn restriction to Mob Leader's Crown

--- GremlinsCode/Relics/MobLeadersCrown.cs
+++ GremlinsCode/Relics/MobLeadersCrown.cs
@@ -1,7 +1,9 @@
 using BaseLib.Utils;
 using Gremlins.GremlinsCode.Core;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
@@ -15,6 +17,8 @@ namespace Gremlins.GremlinsCode.Relics;
 [Pool(typeof(GremlinsRelicPool))]
 public class MobLeadersCrown : GremlinsRelicModel
 {
+    private bool _triggeredThisTurn;
+
     public MobLeadersCrown() : base(RelicRarity.Starter)
     {
         WithEnergy(1);
@@ -23,10 +27,21 @@ public class MobLeadersCrown : GremlinsRelicModel
 
     public override async Task AfterShuffle(PlayerChoiceContext ctx, Player shuffler)
     {
-        if (shuffler != Owner) return;
+        if (shuffler != Owner || _triggeredThisTurn) return;
         Flash();
         await PlayerCmd.GainEnergy(DynamicVars.Energy.BaseValue, Owner);
         await CardPileCmd.Draw(ctx, Owner);
         await GremlinsCmd.SwapToNext(ctx, Owner);
+        _triggeredThisTurn = true;
     }
+
+    public override Task BeforeSideTurnStart(PlayerChoiceContext choiceContext, CombatSide side, IReadOnlyList<Creature> participants, ICombatState combatState)
+	{
+		if (!participants.Contains(base.Owner.Creature))
+		{
+			return Task.CompletedTask;
+		}
+		_triggeredThisTurn = false;
+		return Task.CompletedTask;
+	}
 }
